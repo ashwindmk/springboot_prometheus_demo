8eadcd735c3992230081509b47a72a8b8b611eaa
Ashwin <ashwin.dinesh01@gmail.com>
2020-10-22 22:26:07 +0530

    First commit

---
+++ src/main/java/com/ashwin/java/api/StudentController.java
@@ -0,0 +1,24 @@
+package com.ashwin.java.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @GetMapping
+    @RequestMapping("/")
+    public ResponseEntity<JsonNode> get() throws JsonProcessingException {
+        System.out.println("GET: thread: " + Thread.currentThread().getName());
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree("{\"id\": \"132\", \"name\": \"Alice\"}");
+        return ResponseEntity.ok(json);
+    }
+}
